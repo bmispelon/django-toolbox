2d538fbe4efb9008087ce2477ccf69d1f228e384
Baptiste Mispelon <bmispelon@gmail.com>
2011-01-24 12:24:04 +0100

    Added a forms module with a ForcedValueModelForm class.

---
+++ toolbox/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class ForcedValueModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self._forced_values = kwargs.pop('forced', {})
+        super(ForcedValueModelForm, self).__init__(*args, **kwargs)
+    
+    def clean(self, *args, **kwargs):
+        for k, v in self._forced_values:
+            setattr(self.instance, k, v)
+        return super(ForcedValueModelForm, self).clean(*args, **kwargs)
