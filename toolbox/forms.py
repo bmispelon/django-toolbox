bcdaab1c842a42439a5850c17b53efc6d725c04e
Baptiste Mispelon <bmispelon@gmail.com>
2011-03-25 15:35:50 +0100

    Added an ExtraCleanModelForm.

--- toolbox/forms.py
+++ toolbox/forms.py
@@ -9,3 +9,18 @@ class ForcedValueModelForm(forms.ModelForm):
         for k, v in self._forced_values.items():
             setattr(self.instance, k, v)
         return super(ForcedValueModelForm, self).clean(*args, **kwargs)
+
+class ExtraCleanModelForm(forms.ModelForm):
+    def _mark_error(self, field, error, clean_data):
+        self._errors[field] = self.error_class([error])
+        del clean_data[field]
+    
+    
+    def clean(self):
+        cleaned = self.cleaned_data
+        for name, field in self.fields.items():
+            try:
+                getattr(self, 'x_clean_%s' % name)(cleaned)
+            except AttributeError:
+                pass
+        return cleaned
