a123626314a6f5be0594b49c298043818aef4e70
Baptiste Mispelon <bmispelon@gmail.com>
2011-05-13 17:00:25 +0200

    Added IntegerChoiceField and MultipleIntegerChoiceField to the fields module.

--- toolbox/fields.py
+++ toolbox/fields.py
@@ -1,4 +1,4 @@
-from django.forms.fields import CharField
+from django.forms.fields import CharField, TypedChoiceField, TypedMultipleChoiceField
 from toolbox.text import smartish_split
 from django.utils.encoding import smart_unicode
 
@@ -6,3 +6,22 @@ class MultipleKeywordsSearchField(CharField):
     # TODO: stopwords
     def to_python(self, value):
         return [smart_unicode(bit) for bit in smartish_split(value)]
+
+
+class IntegerChoiceField(TypedChoiceField):
+    def __init__(self, *args, **kwargs):
+        new_kwargs = {
+            'coerce': int,
+            'empty_value': 0,
+        }
+        new_kwargs.update(kwargs)
+        super(IntegerChoiceField, self).__init__(*args, **new_kwargs)
+
+class MultipleIntegerChoiceField(TypedMultipleChoiceField):
+    def __init__(self, *args, **kwargs):
+        new_kwargs = {
+            'coerce': int,
+            'empty_value': 0,
+        }
+        new_kwargs.update(kwargs)
+        super(MultipleIntegerChoiceField, self).__init__(*args, **new_kwargs)
