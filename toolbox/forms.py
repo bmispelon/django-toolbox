5725aa83a79f6f72ce4d846deb17d1a2f54860d6
Baptiste Mispelon <bm@m2bpo.com>
2011-06-01 16:37:09 +0200

    Added MultiForm.

--- toolbox/forms.py
+++ toolbox/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 class ForcedValueModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -24,3 +25,70 @@ class ExtraCleanModelForm(forms.ModelForm):
             except AttributeError:
                 pass
         return cleaned
+
+
+class MultiForm(object):
+    """
+    A ducktyped form class that wraps several forms into one entity.
+    A MultiForm class should have a forms property that is an iterable of
+    (form_prefix, form_class) tuples.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        self.prefix = kwargs.pop('prefix', None)
+        self.initial = kwargs.pop('initial', None)
+        self.instance = kwargs.pop('instance', None)
+        extra_kwargs = kwargs.pop('extra_kwargs', {})
+
+        _l = []
+        for name, form_class in self.forms:
+            k = {
+                'prefix': self.get_prefix(name),
+                'initial': self.get_initial(name),
+            }
+            instance = self.get_instance(name)
+            if instance is not None:
+                k['instance'] = instance
+            
+            k.update(kwargs)
+            k.update(extra_kwargs.get(name, {}))
+            
+            _l.append((name, form_class(*args, **k)))
+        
+        self.forms = _l
+    
+    def get_prefix(self, name):
+        if self.prefix is None:
+            return name
+        else:
+            return '%s-%s' % (global_prefix, name)
+    
+    def get_initial(self, name):
+        if self.initial is None:
+            return None
+        return self.initial.get(name)
+    
+    def get_instance(self, name):
+        if self.instance is None:
+            return None
+        return self.instance.get(name)
+    
+    def is_valid(self):
+        return all([f.is_valid() for _x, f in self.forms])
+    
+    def clean(self):
+        [f.clean() for _x, f in self.forms]
+    
+    @property
+    def cleaned_data(self):
+        return dict((name, f.cleaned_data) for name, f in self.forms)
+    
+    def save(self):
+        # Children class should implement this
+        raise NotImplementedError
+    
+    def as_ul(self):
+        return mark_safe('\n'.join(f.as_ul() for _x, f in self.forms))
+    
+    def __getitem__(self, key):
+        return dict(self.forms)[key]
