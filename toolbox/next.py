5ef01e0b5a1a5ecd0d34b7a3f6ca822514702ed8
Baptiste Mispelon <bm@m2bpo.com>
2012-10-31 09:26:55 +0100

    Refactored and documented toolbox.next.

--- toolbox/next.py
+++ toolbox/next.py
@@ -1,45 +1,58 @@
 from django.core.urlresolvers import reverse
-from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponseRedirect
 from django.views import generic
 
+
 class NextMixin(object):
-    default_next_urlname = None
+    """A mixin for generic CBV that defines get_success_url to take into account
+    a parameter in the request.
+    If that parameter is not present, or is empty, fall back to a default URL.
+    If that default parameter is empty, use the request's referer header.
+    """
+    default_next_urlname = None # For backwards compatibility
+    default_next_url = None # New views should use this attribute with reverse_lazy
     next_field_name = 'next'
     next_context_name = 'next'
     
     def get_default_next_url(self):
-        if self.default_next_urlname is None:
-            return self.request.META['HTTP_REFERER']
-        return reverse(self.default_next_urlname)
+        """Return the fallback URL for when the request contains no redirect
+        parameter. If none is defined, use the referer header.
+        
+        """
+        if self.default_next_urlname:
+            next = reverse(self.default_next_urlname)
+        elif self.default_next_url:
+            next = self.default_next_url
+        else:
+            next = self.request.META['HTTP_REFERER']
+        return next
     
     def get_success_url(self):
+        """Look for a redirect URL in the request parameters (GET or POST)."""
         next = self.request.REQUEST.get(self.next_field_name)
         return next or self.get_default_next_url()
     
     def redirect(self):
+        """A utility method that returns a 302 HTTP response."""
         return HttpResponseRedirect(self.get_success_url())
+    
+    def get_context_data(self, **kwargs):
+        """Add the computed next URL to the context."""
+        context = super(NextMixin, self).get_context_data(**kwargs)
+        context[self.next_context_name] = self.get_success_url()
+        return context
 
+
+# The following are left for backwards-compatibility reasons.
+# New views should just use the mixin.
 
 class CreateView(NextMixin, generic.CreateView):
-    def get_context_data(self, **kwargs):
-        context = super(CreateView, self).get_context_data(**kwargs)
-        context[self.next_context_name] = self.get_success_url()
-        
-        return context
+    pass
 
 
 class UpdateView(NextMixin, generic.UpdateView):
-    def get_context_data(self, **kwargs):
-        context = super(UpdateView, self).get_context_data(**kwargs)
-        context[self.next_context_name] = self.get_success_url()
-        
-        return context
+    pass
 
 
 class DeleteView(NextMixin, generic.DeleteView):
-    def get_context_data(self, **kwargs):
-        context = super(DeleteView, self).get_context_data(**kwargs)
-        context[self.next_context_name] = self.get_success_url()
-        
-        return context
+    pass
